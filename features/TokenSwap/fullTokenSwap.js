36bd2b367e7fe2d99155e0329aee71442c166023
Ben Medeiros <benmedeiros3@gmail.com>
2024-12-15 19:38:14 -0500

    Refactor fullCsvTokenSwap function to remove debug logs and add scanCsvTiming for performance measurement

--- features/TokenSwap/fullTokenSwap.js
+++ features/TokenSwap/fullTokenSwap.js
@@ -8,7 +8,7 @@ const path = require('path');
 function fullCsvTokenSwap(csvString) {
     const rows = csvString.split('\r\n');
     const result = rows.map(row => row.split(','));
-    console.log(result);
+    // console.log(result);
 
     const tokenMap = new Map();
     result.forEach(row => {
@@ -17,11 +17,28 @@ function fullCsvTokenSwap(csvString) {
         });
     });
 
-    console.log([...tokenMap.entries()].sort((a, b) => b[1] - a[1]));
+    // console.log([...tokenMap.entries()].sort((a, b) => b[1] - a[1]));
+    const totalKeyLength = [...tokenMap.keys()].reduce((sum, key) => sum + key.length, 0);
+    console.log('Total sum of key lengths:', totalKeyLength);
+
+    return tokenMap;
 }
+
+function scanCsvTiming(csvString) {
+    const start = process.hrtime();
+    for (let i = 0; i < csvString.length; i++) {
+        // csvString[i] = csvString[i];
+    }
+    const end = process.hrtime(start);
+    console.log(`scanCsvTiming: ${end[0]}s ${end[1] / 1000000}ms`);
+}
+
 
 
 const csvFilePath = path.join(__dirname, 'data/003.csv');
 const csv = fs.readFileSync(csvFilePath, 'utf8');
+console.log(csv.length);
+scanCsvTiming(csv);
 
-fullCsvTokenSwap(csv);
+
+const tokenMap = fullCsvTokenSwap(csv);
